a8c9993eb3e8aa63a68172bc9d93eab5092bcb41
ellathy1 <mahmoudellathy1@gmail.com>
2023-10-12 17:06:33 +0200

    .

--- test01.py
+++
@@ -1,6 +0,0 @@
-n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-
-x = "".join(map(str, n[:3]))
-
-s = "(" + "".join(map(str, n[:3])) + ") " + "".join(map(str, n[3:6])) + "-" + "".join(map(str, n[6:]))
-print (s)
