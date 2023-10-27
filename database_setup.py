36f9c099d75e6438fcbe7f59b5e233351cb9adc9
Mahmoud Mahdy <mahmoudellathy1@gmail.com>
2023-10-27 09:36:09 +0200

    .

--- database_setup.py
+++ database_setup.py
@@ -1,39 +1,61 @@
+from dotenv import load_dotenv
+import os
 import pymysql
+import json
 
-host = "3306"
-user = "root"
-password = "password"
-database = "your_database"
+#import products list
+with open("data/products_list.json", 'r') as file:
+    products_list = json.load(file)
 
-# Establish a connection to the MySQL server
+load_dotenv()
+host_name = os.environ.get("mysql_host")
+user_name = os.environ.get("mysql_user")
+user_password = os.environ.get("mysql_pass")
+database_name = os.environ.get("mysql_db")
+
+##? Establish a connection to the MySQL server
 try:
     conn = pymysql.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
+        host=host_name,
+        user=user_name,
+        password=user_password,
+        database=database_name
     )
-
+    #test
     if conn.open:
         print("Connected to the MySQL database")
 
     # Create a cursor object to interact with the database
     cursor = conn.cursor()
+    
+    
+    ##? Execute SQL queries using the cursor
+    #? making the table
+    # cursor.execute("""CREATE TABLE IF NOT EXISTS products_table (
+    # ProductsID int NOT NULL AUTO_INCREMENT,
+    # ProductsName varchar(255) NOT NULL,
+    # ProductPrice FLOAT NOT NULL,
+    # PRIMARY KEY (ProductsID));""")
+    
+    #? inserting the data
+    for product in products_list:
+        cursor.execute("""INSERT INTO products_table (ProductsName, ProductPrice)
+            VALUES (%s, %s)
+        """, (product["name"], product["price"]))
+        print(product)
+    
+    # Commit the changes to the database
+    conn.commit()
+    print("Data inserted successfully.")
 
-    # Execute SQL queries using the cursor
-    # For example, fetching data from a table:
-    cursor.execute("SELECT * FROM your_table")
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
 
 except pymysql.Error as err:
     print(f"Error: {err}")
+
 
 finally:
     if 'conn' in locals() and conn.open:
         cursor.close()
         conn.close()
-        print("MySQL connection is closed")kuhuhk
+        print("MySQL connection is closed")
         
