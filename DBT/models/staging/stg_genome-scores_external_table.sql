bef3671ccfe977e6605d35ec2dd14f32036a08f1
Mahmoud Mahdy <mahmoudellathy1@gmail.com>
2024-06-07 00:51:22 +0000

    staging

--- DBT/models/staging/stg_genome-scores_external_table.sql
+++ DBT/models/staging/stg_genome-scores_external_table.sql
@@ -1,20 +1,18 @@
-with 
+with genome_score as
 
-source as (
-
-    select * from {{ source('staging', 'genome-scores_external_table') }}
-
-),
-
-renamed as (
-
-    select
-        movieid,
-        tagid,
-        relevance
-
-    from source
-
+(
+    select *,
+    row_number() over(partition by movieid, tagid ) as rn
+    from {{ source('staging', 'genome-scores_external_table') }}
+    where movieid is not null
 )
 
-select * from renamed
+select * 
+from genome_score
+where rn = 1
+
+-- {% if var('is_test_run', default=true) %}
+
+--   limit 100
+
+-- {% endif %}
