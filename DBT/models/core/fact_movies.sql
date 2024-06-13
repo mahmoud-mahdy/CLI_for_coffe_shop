47edb67a71eb2a3daf83dc1cd4b37764a112ea25
Mahmoud Mahdy <mahmoudellathy1@gmail.com>
2024-06-13 16:16:11 +0000

    removing moves with less than 20 reviews removing movies with title or release year

--- DBT/models/core/fact_movies.sql
+++ DBT/models/core/fact_movies.sql
@@ -19,19 +19,33 @@ select *
 from {{ ref('stg_ratings') }}
 ),
 
-tags as
-(
-select *
-from {{ ref('stg_tags') }}
+-- removing all movies with less than 20 reviews (unreliable data)
+rating_transformation as
+(SELECT
+    movieId,
+    count(rating) as review_count
+FROM rating
+
+GROUP BY movieId
+
+HAVING COUNT(rating.rating) >= 20
+ORDER BY review_count
 )
 
-select rating.userId, rating.rating, movies.title as Movie_title, movie_release_year, movies.genres, Rating.timestamp as rating_time, tags.user_tag, 
+select rating.movieId, rating.userId, rating.rating, movies.title as Movie_title, movies.movie_release_year, movies.genres, Rating.timestamp as rating_time,  
 links.imdbid, links.tmdbid
 
-
 from rating
-left join movies on rating.movieId = movies.movieId
+
+inner join movies on rating.movieId = movies.movieId
 left join links on movies.movieId = links.movieId
-left join tags on rating.userId = tags.userId
+inner join rating_transformation on rating_transformation.movieId = rating.movieId
+
+where 
+    -- removing all moves doesnt have title or release year (all remaining moves have both title and release year)
+    movies.title != ''
+    and
+    movies.movie_release_year is not null
+
 
 -- dbt build --select <model.sql> --vars '{'is_test_run': 'false'}'
